b8460f3cb90bab4707947f3bfc2caca7d3155305
Carter Blackie <carterblackie55@gmail.com>
2025-02-14 19:23:23 -0500

    Setting up main with a menu
    Default menu system for main

---
+++ Scripts/main.py
@@ -0,0 +1,34 @@
+import functions
+def main():
+    """
+    -------------------------------------------------------
+    Menu system to choose between functions.
+    -------------------------------------------------------
+    """
+    while True:
+        print("\nMenu:")
+        print("1. Retrieve a specific customer record")
+        print("2. Find the customer with the highest balance")
+        print("3. Append max number to file")
+        print("4. Copy file contents")
+        print("5. Copy first N lines from one file to another")
+        print("6. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+main()
